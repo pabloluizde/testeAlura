f6214c02dca2a4541fe3f7a16fbad800a630cfa3
Pablo Luiz <pablo2006_luiz@hotmail.com>
2022-06-13 14:40:13 -0300

    commit
    add alert details

--- testeAlura/Controllers/RefeicoesTableViewController.swift
+++ testeAlura/Controllers/RefeicoesTableViewController.swift
@@ -24,7 +24,29 @@ class RefeicoesTableViewController: UITableViewController, AddRefeicaoDelegate{
         celula.textLabel?.text = "Prato: \(refeicao.nome)"
         celula.detailTextLabel?.text = "Satisfação: \(refeicao.felicidade)"
         
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(mostrarDetalhes(_:)))
+        celula.addGestureRecognizer(longPress)
+        
         return celula
+    }
+    
+    @objc func mostrarDetalhes(_ gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .began {
+            let celula = gesture.view as! UITableViewCell
+            
+            guard let indexPath = tableView.indexPath(for: celula) else {
+                return
+            }
+            let refeicao = refeicoes[indexPath.row]
+            
+            let alerta = UIAlertController(title: refeicao.nome, message: "Satisfação: \(refeicao.felicidade)", preferredStyle: .alert)
+            
+            let botaoCancelar = UIAlertAction(title: "cancelar", style: .cancel, handler: nil)
+            
+            alerta.addAction(botaoCancelar)
+            
+            present(alerta, animated: true, completion: nil)
+        }
     }
     
     func add(_ refeicao: Refeicao) {
